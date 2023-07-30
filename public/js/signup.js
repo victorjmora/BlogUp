fc67867446200594f582ef83bd2bd6af8aff5e5f
victormora1 <victorjmora1@gmail.com>
2023-07-30 19:20:40 -0400

    some changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,14 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-signup');
+    //const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup');
   
-    if (name && email && password) {
+    if (username && password) {
       const response = await fetch('/api/user/signup', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({name: username.value.trim(), username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
